afc270460cfef426101683d279473e980252e479
erhemdiputra <erhem.diputra@tokopedia.com>
2018-08-29 21:43:48 +0700

    add exception handling section

---
+++ Hackerrank/6-Exception-Handling/2-Exception-Handling/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class MyCalculator {
+    public int power(int n, int p) throws Exception {
+        if (n < 0 || p < 0) {
+            throw new Exception("n or p should not be negative.");
+        }
+
+        if (n == 0 && p == 0) {
+            throw new Exception("n and p should not be zero.");
+        }
+
+        int res = 1;
+        for (int i = 1; i <= p; i++) {
+            res *= n;
+        }
+
+        return res;
+    }
+}
+
+public class Solution {
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        while (in .hasNextInt()) {
+            int n = in .nextInt();
+            int p = in .nextInt();
+            
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
